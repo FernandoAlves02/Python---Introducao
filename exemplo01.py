0e622c4b0546ba010bd8581250f8af2dac0526ec
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-06-10 21:28:07 -0300

    Initial Commit

---
+++ exemplo01.py
@@ -0,0 +1,10 @@
+numero1 = int(input("Digite um valor: "))
+numero2 = int(input("Digite um valor: "))
+
+resposta = numero1 + numero2
+print("Resposta ", resposta)
+
+numero1, numero2 = numero2, numero1
+
+print("Numero 1: ", numero1)
+print("Numero 2: ", numero2)
